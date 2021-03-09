9a91d83238aad224a38577c353a79491a86bcd86
irinaBalib <irinaBalib@users.noreply.github.com>
2021-03-09 16:12:38 +0200

    Add project files.

---
+++ CINEMA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CINEMA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
