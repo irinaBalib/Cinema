41b35296373103b1abcf06cc1860c3795501c23b
irinaBalib <irinaBalib@users.noreply.github.com>
2021-03-09 16:19:50 +0200

    1

---
+++ CINEMA/Managers/MovieManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CINEMA.Managers
+{
+    class MovieManager
+    {
+    }
+}
---
+++ CinemaWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CinemaWEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
